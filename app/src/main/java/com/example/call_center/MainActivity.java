46f86dd3014879c89b7152f7df06f0a49904ac6e
khodaid <khoirulhudaid@gmail.com>
2021-11-02 18:28:48 +0700

    pertama

---
+++ app/src/main/java/com/example/call_center/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.call_center;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button damkar, polisi, basarnas, bnbp, ambulan;
+
+        damkar = (Button)findViewById(R.id.btnDamkar);
+        damkar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Pemadam.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/call_center/Pemadam.java
@@ -0,0 +1,21 @@
+package com.example.call_center;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Pemadam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemadam);
+
+        Button btn_call;
+
+//        btn_call = (Button)findViewById(R.id.btnCall);
+//        btn_call.setBackgroundColor(Color.rgb(0,255,85));
+    }
+}
